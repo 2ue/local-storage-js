aa99b6b35327a0e7e94b4cefb51a95c0ef5a8a70
2ue <jie746635835@163.com>
2017-04-28 11:18:48 +0800

    增加了汉字转数字

--- src/lib/tsNum.js
+++ src/lib/tsNum.js
@@ -8,7 +8,7 @@ var UNIT_ARRAY = ['千','百','十'];
 var POINT = '点';
 var NUM_ARRAY = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'];
 var NUM_UNIT_ARRAY = ['万', '亿', '兆', '京', '垓', '秭', '穰', '沟', '涧', '正', '载', '极', '恒河沙', '阿僧祗', '那由他', '不可思议', '无量', '大数'];
-//删除连续重复字符
+//匹配连续重复字符
 var REG_DEL_REPEAT = /(.)\1+/g;
 //正向四位分割字符串
 var REG_SPLIT_LEN = /(\d{4}(?=\d)(?!\d+\.|$))/g;
@@ -19,7 +19,7 @@ var REG_SPLIT_LEN_R = /(\d{1,4})(?=(?:\d{4})+(?!\d))/g;
 function dealNum(_NUM){
     if(!_NUM || isNaN(_NUM)) return [];
     return _NUM.split('.');
-}
+};
 
 //每四位分割成一组
 function splitNum(_NUM,_len,_type){
@@ -49,7 +49,7 @@ function switchNum(_NUM,_index){
     });
     return res.join('').replace(REG_DEL_REPEAT,'$1');
 
-}
+};
 //转换小数部分
 function switchDecimal(_NUM){
     if(!_NUM) return;
@@ -64,7 +64,7 @@ function switchDecimal(_NUM){
         
     });
     return res.join('');
-}
+};
 
 //拼接
 function jionNum (_NUM) {
@@ -152,7 +152,7 @@ function switchDecimalHz(_HZ){
         
     });
     return res.join('');
-}
+};
 
 function jionHz(_HZ){
     if(!_HZ || _HZ == NUM_ARRAY[0]) return 0;
@@ -165,7 +165,7 @@ function jionHz(_HZ){
         res = res + '' + temp;
     };
     return res + decimalPart;
-}
+};
 console.log(jionHz('三十穰零五千六百七十四京七千七百四十兆二千三百亿二千三百零五万零七百八十九点八八九九零九'))
 //期望：300000000056747740230023050789.889909
 //实际：30000000005674774023002305789.889909
