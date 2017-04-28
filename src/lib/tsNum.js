362b7fd704935f650a5c0fd1ace4d0420b053d51
2ue <jie746635835@163.com>
2017-04-28 11:01:29 +0800

    增加了汉字转数字

--- src/lib/tsNum.js
+++ src/lib/tsNum.js
@@ -5,6 +5,7 @@
 ;
 //默认配置
 var UNIT_ARRAY = ['千','百','十'];
+var POINT = '点';
 var NUM_ARRAY = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'];
 var NUM_UNIT_ARRAY = ['万', '亿', '兆', '京', '垓', '秭', '穰', '沟', '涧', '正', '载', '极', '恒河沙', '阿僧祗', '那由他', '不可思议', '无量', '大数'];
 //删除连续重复字符
@@ -49,7 +50,7 @@ function switchNum(_NUM,_index){
     return res.join('').replace(REG_DEL_REPEAT,'$1');
 
 }
-
+//转换小数部分
 function switchDecimal(_NUM){
     if(!_NUM) return;
     var res = [];
@@ -81,13 +82,96 @@ function jionNum (_NUM) {
         }
     };
 
-    return reslt.replace(REG_DEL_REPEAT,'$1') + (!numArray[1] ? '' : ('点' + switchDecimal(numArray[1])));
+    return reslt.replace(REG_DEL_REPEAT,'$1') + (!numArray[1] ? '' : (POINT + switchDecimal(numArray[1])));
         
 };
 
 console.log(jionNum('300000000056747740230023050789.889909000'));
+//三十穰零五千六百七十四京七千七百四十兆二千三百亿二千三百零五万零七百八十九点八八九九零九
+
+//转换成数字
+//分割整数和小数部分
+function dealHz(_HZ){
+    if(!_HZ) return [];
+    return _HZ.split(POINT);
+};
+//分割成组
+function splitHz(_HZ){
+    if(!_HZ) return [0];
+    //去掉整数部分所有的'零'，然后分割
+    _HZ = _HZ.replace(/零/g,'').split('');
+    var res = [];
+    var temp = '';
+    var location = -10000;//设置一个超大数
+    for(let i = 0; i < _HZ.length; i++){
+        var thisLocation = NUM_UNIT_ARRAY.indexOf(_HZ[i]);
+        if(thisLocation >= 0){
+            
+            if(thisLocation - location < -1){
+                for(let loc = 1; loc < location - thisLocation; loc ++){
+                    res.push(NUM_ARRAY[0]);
+                }  
+            };
+            res.push(temp);
+            temp = '';
+            location = thisLocation;
+            
+        }else{
+            temp += _HZ[i];
+            if(i == _HZ.length - 1) res.push(temp);
+        }
+    };
+    return res;
+};
+
+function switchHz(_HZ){
+    if(!_HZ) return '';
+    if(_HZ == NUM_ARRAY[0]) return '0000';
+    _HZ = _HZ.split('');
+    var res = 0;
+    var temp = 0;
+    for(let i = 0; i < _HZ.length; i++){
+        if(i % 2 == 0){
+            temp = NUM_ARRAY.indexOf(_HZ[i]);
+            if(i == _HZ.length - 1) res += temp;
+        }else{
+            var z = 3 - UNIT_ARRAY.indexOf(_HZ[i]);
+            res += (temp * Math.pow(10,z));
+        }
+    }
+    return res;
+
+};
+//转换小数部分
+function switchDecimalHz(_HZ){
+    if(!_HZ) return '';
+    _HZ = _HZ.split('');
+    var res = ['.'];
+    _HZ.map(function(n){
+        res.push(NUM_ARRAY.indexOf(n));
+        
+    });
+    return res.join('');
+}
+
+function jionHz(_HZ){
+    if(!_HZ || _HZ == NUM_ARRAY[0]) return 0;
+    _HZ = dealHz(_HZ);
+    var HZ_ARRY = splitHz(_HZ[0]);
+    var decimalPart = switchDecimalHz(_HZ[1]);
+    var res = '';
+    for(let i = 0; i < HZ_ARRY.length; i++){
+        var temp = switchHz(HZ_ARRY[i]);
+        res = res + '' + temp;
+    };
+    return res + decimalPart;
+}
+console.log(jionHz('三十穰零五千六百七十四京七千七百四十兆二千三百亿二千三百零五万零七百八十九点八八九九零九'))
+//期望：300000000056747740230023050789.889909
+//实际：30000000005674774023002305789.889909
 
 //向外提供接口
 module.exports = {
-    tsNum: jionNum
+    tsNum: jionNum,
+    tsHz: jionHz
 };
